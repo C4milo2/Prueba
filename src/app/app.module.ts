a5b2ba2af54b74ae4423e1e032900c0ede571529
C4milo2 <87783495+C4milo2@users.noreply.github.com>
2021-08-08 21:07:39 -0500

    subiendo angular a github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BodyComponent } from './body/body.component';
+import { ContenidoComponent } from './contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BodyComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule
